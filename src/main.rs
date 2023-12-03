686a21ebdf5fa12af86d90dbba7280f6ffed681e
Elli Beres <ellifteria@gmail.com>
2023-12-02 20:39:44 -0600

    Initial complexity analysis

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,10 @@ fn memory_profile_add()
 
     device.read_program("examples/add.asm");
 
-    for j in 1..=30
+    for i in [0x1, 0x8, 0x80, 0x8000]
     {
-        eprintln!("j={}", j);
+        eprintln!("i={}", i);
         eprintln!("{:->36}","");
-        let i = 1 << j;
         device.clear_device_execution_memory();
         device.load_input_variable("i00", i-1);
         device.load_input_variable("i01", 1);
@@ -35,11 +34,10 @@ fn memory_profile_lin_add()
 
     device.read_program("examples/lin-add.asm");
 
-    for j in 1..=30
+    for i in [0x1, 0x8, 0x80, 0x8000]
     {
-        eprintln!("j={}", j);
+        eprintln!("i={}", i);
         eprintln!("{:->36}","");
-        let i = 1 << j;
         device.clear_device_execution_memory();
         device.load_input_variable("i00", i-1);
         device.load_input_variable("i01", 1);
@@ -59,15 +57,14 @@ fn memory_profile_pal_add()
 
     device.read_program("examples/pal-add.asm");
 
-    for j in 1..=30
+    for i in [0x1, 0x8, 0x80, 0x8000]
     {
-        eprintln!("j={}", j);
+        eprintln!("i={}", i);
         eprintln!("{:->36}","");
-        let i = (1 << j ) | (1u32);
         device.clear_device_execution_memory();
-        device.load_input_variable("i00", i);
+        device.load_input_variable("i00", i | (1u32));
         device.load_input_variable("i01", 0);
-        device.load_input_variable("i02", i);
+        device.load_input_variable("i02", i | (1u32));
         device.execute_program(None);
         let (input_used, touched_flags, touched_registers) = device.count_touched_memory();
         println!("{},{},{}", Variable::u32_to_bits(i).len(), input_used, touched_flags + touched_registers);
